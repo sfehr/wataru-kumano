eebc01bebbde2b7aadecefba9c2338877efa7d38
Sebastian Fehr <sebastianfehr@Sebastians-iMac.local>
2020-08-03 17:19:53 +0900

    - mobile navigation bug fix (distinguish touch or click event)
    - project / section title change ( use padding instead of margin )

--- wp-content/themes/wataru_kumano/js/wk-scripts.js
+++ wp-content/themes/wataru_kumano/js/wk-scripts.js
@@ -4,12 +4,13 @@
  *
  * CONTENT OBJECT CONSTRUCTOR
  * INDEXING PROJECTS
- * SCROLL TO ANCHOR FUNCTION	
+ * SCROLL TO ANCHOR FUNCTION
  * TOP SCROLL STATE 
  * NAV ACTIVE STATE
  * NAV BAR HOVER INTERACTION
  * UPDATE CONTENT
  * IMAGE CLASS RIGHT
+ * GET OFFSET-TOP VALUE
  *  
 */
 
@@ -18,7 +19,7 @@ jQuery( document ).ready( function( $ ) {
 	// VARIABLES
 	var projects = {};
 	var project_index = 0;
-	var offset_top = 200;
+	var offset_top = 360; // reusing this variable to adjust scroll into viewport with initial value for desktop site
 	var nav_content_container = $( '#nav-bar .nav-content-container' );
 	var nav_site_logo = $( '#nav-bar .logo' );
 	var nav_project_title = $( '#nav-bar .project-title' );
@@ -55,7 +56,7 @@ jQuery( document ).ready( function( $ ) {
 	});
 	
 	update_content();
-	
+	get_offset_top();
 
 	
 	// SCROLL TO ANCHOR FUNCTION	
@@ -64,12 +65,13 @@ jQuery( document ).ready( function( $ ) {
 		var dest = $( this ).attr( 'href' ); 
 		
 		$( 'html, body' ).animate({ 
-			scrollTop: $( dest ).offset().top + 2 }, 'smooth' ); 
+			scrollTop: $( dest ).offset().top + 2 + offset_top }, 'smooth' ); 
 	});
 	
 	
 	
 	// TOP SCROLL STATE
+/*	
 	$( window ).on( 'scroll', function(){	
 		if( $( window ).scrollTop() > offset_top ){
 
@@ -79,14 +81,15 @@ jQuery( document ).ready( function( $ ) {
 			$( 'body' ).removeClass( 'detached' );
 		}
 	});
-	
+*/	
 	
 	
 	// NAV ACTIVE STATE
 	let mainNavLinks = document.querySelectorAll( '.anchor-link' );
 
 	window.addEventListener( 'scroll', event => {
-		let fromTop = window.scrollY;
+		let fromTop = window.scrollY;		
+		
 		mainNavLinks.forEach( link => {
 			let section = document.querySelector( link.hash );
 
@@ -106,51 +109,83 @@ jQuery( document ).ready( function( $ ) {
 	
 
 	// NAV BAR HOVER INTERACTION
-	// Expand the nav bar on hover (or click for mobile)
-	nav_site_logo.add( nav_project_title ).add( nav_project_type ).add( nav_project_year ).on( 'mouseenter click', function( e ){
+	// click
+	nav_site_logo.add( nav_project_title ).add( nav_project_type ).add( nav_project_year ).on( 'click', function(){
 		
 		// collapse/close when active nav bar element is clicked.
-		if( e.type == 'click' ){ 
 		
-			if( $( this ).is( nav_site_logo ) && $( '#nav-bar' ).hasClass( 'expand-profile' ) ){ 
+		// Profile
+		if( $( this ).is( nav_site_logo ) ){
+			if( $( '#nav-bar' ).hasClass( 'expand-profile' ) ){ 
 				sf_collapse_nav_bar();
-				return;
 			}
-			if( $( this ).is( nav_project_title ) && $( '#nav-bar' ).hasClass( 'expand-title' ) ){ 
-				sf_collapse_nav_bar();
-				return;
-			}
-			if( ( $( this ).is( nav_project_type ) && $( '#nav-bar' ).hasClass( 'expand-navigation' ) ) || ( $( this ).is( nav_project_year ) && $( '#nav-bar' ).hasClass( 'expand-navigation' ) ) ){ 
-				sf_collapse_nav_bar();
-				return;
-			}					
-			
-		}			
+			else{
+				$( '#nav-bar' ).addClass( 'expand-profile' );
+				$( '#nav-bar' ).removeClass( 'expand-title expand-navigation' );
+			}				
+		}
 				
+		// Project Title
+		if( $( this ).is( nav_project_title ) ){
+			if( $( '#nav-bar' ).hasClass( 'expand-title' ) ){ 
+				sf_collapse_nav_bar();
+			}
+			else{
+				$( '#nav-bar' ).addClass( 'expand-title' );
+				$( '#nav-bar' ).removeClass( 'expand-profile expand-navigation' );
+				nav_content_container.html( projects[ project_index ].content );
+			}				
+		}
+				
+		// Navigation	
+		if( $( this ).is( nav_project_type ) || $( this ).is( nav_project_year ) ){
+				
+			if( $( '#nav-bar' ).hasClass( 'expand-navigation' ) ){ 
+				sf_collapse_nav_bar();
+			}
+			else{
+				$( '#nav-bar' ).addClass( 'expand-navigation' );
+				$( '#nav-bar' ).removeClass( 'expand-title expand-profile' );			
+			}				
+		}
+	});			
+	
+	// mouseenter	
+	nav_site_logo.add( nav_project_title ).add( nav_project_type ).add( nav_project_year ).on( 'mouseenter', function(){
+		
+		// abort on touch device
+		if( matchMedia( '(pointer:coarse)' ).matches ){
+			return;
+		}
+		
 		// Add expand class, update content
+		// Profile
 		if( $( this ).is( nav_site_logo ) ){
 			$( '#nav-bar' ).addClass( 'expand-profile' );
 			$( '#nav-bar' ).removeClass( 'expand-title expand-navigation' );
-		}		
+		}
+		// Project Title
 		if( $( this ).is( nav_project_title ) ){
 			$( '#nav-bar' ).addClass( 'expand-title' );
 			$( '#nav-bar' ).removeClass( 'expand-profile expand-navigation' );
 			nav_content_container.html( projects[ project_index ].content );
 		}
+		// Navigation
 		if( $( this ).is( nav_project_type ) || $( this ).is( nav_project_year ) ){
 			$( '#nav-bar' ).addClass( 'expand-navigation' );
 			$( '#nav-bar' ).removeClass( 'expand-title expand-profile' );
-		}		
+		}
 		
 	});
 	
 	// Collapse the nav bar on leave with a delay of 500ms (for smooth animation in css)
 	$( '#nav-bar' ).on( 'mouseleave', sf_collapse_nav_bar );
 	
+	// delay the class update for moving down animation
 	function sf_collapse_nav_bar(){
 		setTimeout( function(){
 			$( '#nav-bar' ).removeClass( 'expand-title expand-profile expand-navigation' );
-		}, 250 );				
+		}, 250 );						
 	}
 
 	
@@ -180,7 +215,29 @@ jQuery( document ).ready( function( $ ) {
 		if( 2 == rand ){
 			$( this ).addClass( 'center' );
 		}		
-	});	
+	});
+
 	
+	
+	// GET OFFSET-TOP VALUE
+	function get_offset_top(){
+
+		var new_value = parseFloat( jQuery( site_sections ).eq( 2 ).css( 'padding-top' ) );
+		offset_top = ( '0px' != new_value ) ? new_value : offset_top;
+		
+	}
+
+	
+	
+	// RESIZE EVENTS
+	var resizing;
+	window.onresize = function() {
+
+		clearTimeout( resizing );
+
+		resizing = setTimeout( function() {
+			get_offset_top();
+		}, 100);
+	};	
 
 });
