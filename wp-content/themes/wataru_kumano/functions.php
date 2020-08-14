eebde085115c6f67ebd6754f8238b2ec88ea8173
Sebastian Fehr <sebastianfehr@Sebastians-iMac.local>
2020-08-14 17:10:33 +0900

    - adding google tag manager in head tag

--- wp-content/themes/wataru_kumano/functions.php
+++ wp-content/themes/wataru_kumano/functions.php
@@ -243,6 +243,17 @@ add_filter( 'template_include', 'wk_choose_template', 99 );
  */
 function wk_custom_head() {
 
+	// GMT
+	echo "
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-WP7C7CJ');</script>
+		<!-- End Google Tag Manager -->	
+	";
+	
 	// TYPEFACE (EN)
 	echo '<link rel="stylesheet" href="https://use.typekit.net/wlv6frg.css">';	
 	
